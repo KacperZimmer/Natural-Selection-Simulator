ccd7add475f37dee22d23e2013aee24a0bd79108
Kacper <kacperzimmer@outlook.com>
2025-03-18 17:20:33 +0100

    simple menu working

--- src/SimulationSrc/Simulation.cpp
+++ src/SimulationSrc/Simulation.cpp
@@ -7,54 +7,64 @@
 #include "../../include/EntityFactoryInclude/creatureFactory.h"
 #include "../../include/CreatureIncludes/creatureContainer/CreatureContainer.h"
 
-
-
 void Simulation::run() {
 
-    FoodContainer foodContainer{};
-    foodContainer.generateFood(50   );
+    int creatureCount = 24;
+    int foodCount = 50;
+    bool inMenu = true;
 
-    std::unique_ptr<entityFactory> entityFactorytest = std::make_unique<CreatureFactory>();
-    CreatureContainer creatureContainer{entityFactorytest};
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Selection simulator");
+    SetTargetFPS(60);
 
-    creatureContainer.generateSymmetricaly(24,10.f);
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT,"Selection simulator");
+    while (!WindowShouldClose()) {
+        if (inMenu) {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
 
-    SetTargetFPS(1000);
-    while(!WindowShouldClose()){
+            DrawText("Ustawienia symulacji", SCREEN_WIDTH / 2 - 100, 50, 20, DARKGRAY);
 
-        BeginDrawing();
-        ClearBackground(WHITE);
+            GuiSpinner(Rectangle{SCREEN_WIDTH / 2 - 60, 100, 120, 30}, "Stworzenia:", &creatureCount, 1, 100, true);
+            GuiSpinner(Rectangle{SCREEN_WIDTH / 2 - 60, 150, 120, 30}, "Jedzenie:", &foodCount, 1, 100, true);
 
-        creatureContainer.render();
-        creatureContainer.update(foodContainer);
+            if (GuiButton(Rectangle{SCREEN_WIDTH / 2 - 60, 200, 120, 40}, "Start")) {
+                inMenu = false;
+            }
 
-        if(GuiButton(Rectangle{0.f, SCREEN_HEIGHT - 50,120,50},"turn on vision")){
+            EndDrawing();
+        } else {
+            FoodContainer foodContainer{};
+            foodContainer.generateFood(foodCount);
 
-            creatureContainer.turnOnVision();
+            std::unique_ptr<entityFactory> entityFactorytest = std::make_unique<CreatureFactory>();
+            CreatureContainer creatureContainer{entityFactorytest};
 
+            creatureContainer.generateSymmetricaly(creatureCount, 10.f);
+
+            while (!WindowShouldClose()) {
+                BeginDrawing();
+                ClearBackground(WHITE);
+
+                creatureContainer.render();
+                creatureContainer.update(foodContainer);
+
+                if (GuiButton(Rectangle{0.f, SCREEN_HEIGHT - 50, 120, 50}, "turn on vision")) {
+                    creatureContainer.turnOnVision();
+                }
+                if (GuiButton(Rectangle{120, SCREEN_HEIGHT - 50, 120, 50}, "turn off the vision")) {
+                    creatureContainer.turnOffVision();
+                }
+                if (GuiButton(Rectangle{240, SCREEN_HEIGHT - 50, 120, 50}, "speed up")) {
+                    creatureContainer.inreaseRelativeSpeed();
+                }
+                if (GuiButton(Rectangle{360, SCREEN_HEIGHT - 50, 120, 50}, "slow down")) {
+                    creatureContainer.slowDownRelativeSpeed();
+                }
+
+                foodContainer.renderContainer();
+                EndDrawing();
+            }
         }
-
-        if(GuiButton(Rectangle{120, SCREEN_HEIGHT - 50,120,50},"turn off the vision")){
-
-            creatureContainer.turnOffVision();
-
-        }
-        if(GuiButton(Rectangle{240, SCREEN_HEIGHT - 50,120,50},"speed up")){
-
-            creatureContainer.inreaseRelativeSpeed();
-
-        }
-        if(GuiButton(Rectangle{360, SCREEN_HEIGHT - 50,120,50},"slow down")){
-
-            creatureContainer.slowDownRelativeSpeed();
-
-        }
-
-
-        foodContainer.renderContainer();
-
-        EndDrawing();
     }
-}
 
+    CloseWindow();
+}
